9111add6d6f1dba07f3f1289539afafcbcf1c5e7
Hasanat902 <abulhasanat902@gmail.com>
2022-05-24 13:01:43 +0600

    footer added

--- src/Pages/Home/Capabilities.js
+++ src/Pages/Home/Capabilities.js
@@ -3,13 +3,13 @@ import banner2 from '../../images/products/banner2.jpg';
 
 const Capabilities = () => {
     return (
-        <div class="hero min-h-screen">
-        <div class="hero-content flex-col lg:flex-row">
-            <img src={banner2} class="max-w-lg rounded-lg shadow-2xl" alt='' />
+        <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+            <img src={banner2} className="max-w-lg rounded-lg shadow-2xl" alt='' />
             <div className='ml-12'>
-            <h1 class="text-5xl font-bold">Our Capabilities</h1>
-            <p class="py-6">We Are Restocking manufacturer parts as Quickly as Possible. Come Back 7/30 to Order, more of These Flavors Inspired by the Places You Call Home!</p>
-            <button class="btn btn-primary">Learn More</button>
+            <h1 className="text-5xl font-bold">Our Capabilities</h1>
+            <p className="py-6">We Are Restocking manufacturer parts as Quickly as Possible. Come Back 7/30 to Order, more of These Flavors Inspired by the Places You Call Home!</p>
+            <button className="btn btn-primary">Learn More</button>
             </div>
         </div>
         </div>

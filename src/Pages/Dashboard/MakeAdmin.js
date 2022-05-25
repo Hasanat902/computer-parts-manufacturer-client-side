548d97caf65ea28d2f4cb4bb8aeca0d1d13bb846
Hasanat902 <abulhasanat902@gmail.com>
2022-05-25 22:33:37 +0600

    make admin page created

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import AdminRow from './AdminRow';
 
 const MakeAdmin = () => {
+
+    const {data: users, isLoading} = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+
+    if(isLoading){
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2>Make admin page</h2>
+            <h2 className='text-xl font-bold my-5'>Make admin from users</h2>
+            <div class="overflow-x-auto">
+            <table class="table w-full">
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>Email</th>
+                    <th>Job</th>
+                    <th>Favorite Color</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map(user => <AdminRow key={user._id} user={user}></AdminRow>)
+                    }
+                </tbody>
+            </table>
+            </div>
         </div>
     );
 };
